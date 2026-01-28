80785328462eca566b5d8f3509c72203e1f885b6
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2026-01-28 16:58:53 -0300

    feat: subtracao #1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-export default function somar(num1: number, num2: number): number {
+export function somar(num1: number, num2: number): number {
   return num1 + num2;
 }
 
+export function subtracao(num1: number, num2: number): number {
+  return num1 - num2;
+}
+
 console.log("Função somar executada. Resultado: " + somar(1, 3));
+console.log("Função subtracao executada. Resultado: " + subtracao(3, 1));
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,13 @@
-import somar from "../src/index";
+import { somar, subtracao } from "../src/index";
 
 describe("Somar", () => {
   it("A soma de 1 + 2 deve ser igual a 3", () => {
     expect(somar(1, 2)).toBe(3);
+  });
+});
+
+describe("Subtrair", () => {
+  it("A subtração de 3 - 1 deve ser igual a 2", () => {
+    expect(subtracao(3, 1)).toBe(2);
   });
 });
